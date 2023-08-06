028e998db3d309f0d8e8d1ec2ac7f489bd6d2c39
Konstantin <kbynevskij@yandex.ru>
2023-08-06 17:15:59 +0200

    Fin med 4

--- Exemple4_div/Program.cs
+++ Exemple4_div/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA/numberB);
