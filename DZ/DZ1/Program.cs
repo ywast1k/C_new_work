a5cc88db33fdc6917bd3424e846c1bb392552996
Konstantin <kbynevskij@yandex.ru>
2023-08-08 22:23:11 +0200

    Дз1. большее из двух

---
+++ DZ/DZ1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Здравствуйте! Введите значение 1:  ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение 2:  ");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1>number2)
+ {
+     Console.WriteLine(number1);
+ }
+ else
+ {
+     Console.WriteLine(number2);
+ }
