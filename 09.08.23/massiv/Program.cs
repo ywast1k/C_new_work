c8471d639cc11eedb1c3c712a5824533125d7c38
Konstantin <kbynevskij@yandex.ru>
2023-08-09 22:29:48 +0200

    Jobba med massiv

---
+++ 09.08.23/massiv/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите номер недели");
+int day = int.Parse(Console.ReadLine());
+string[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница" , "Суббота", "Воскресенье"};
+Console.WriteLine(days[day-1]);
