6a94298cccd4a66df940d5978f9e86ca75945a0e
Konstantin <kbynevskij@yandex.ru>
2023-08-09 20:41:37 +0200

    контрольная точка

---
+++ DZ/need_byta/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+int max = 0;
+
+Console.WriteLine("Здравствуйте! Введите 3 значения:  ");
+int[] i= {Convert.ToInt32(Console.ReadLine(),Console.ReadLine(),Console.ReadLine(),Console.ReadLine())};
+
+for (int i = 1; i < 3; i++)
+{
+    max < i;
+    max = i;
+};
+Console.WriteLine(max);
