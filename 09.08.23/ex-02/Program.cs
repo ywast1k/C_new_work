bf94621266bba371f0c3e00ce349ce3b5bea4f19
Konstantin <kbynevskij@yandex.ru>
2023-08-09 19:24:29 +0200

    Проверка числа на квадрат

---
+++ 09.08.23/ex-02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number2*number2 == number)
+{
+    Console.WriteLine($"число {number} является квадратом {number2}"); 
+}
+else
+{
+    Console.WriteLine($"число {number} НЕ является квадратом {number2}"); 
+}
