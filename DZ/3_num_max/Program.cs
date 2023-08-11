5eb92a36d0d089b34711bb1a5db95bbca83f4578
Konstantin <kbynevskij@yandex.ru>
2023-08-11 19:23:33 +0200

    3 числа мах

---
+++ DZ/3_num_max/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 3 значения:  ");
+ int num1 = int.Parse(Console.ReadLine());
+ int num2 = int.Parse(Console.ReadLine());
+ int num3 = int.Parse(Console.ReadLine());
+
+ if(num1>num2)
+ {
+    if(num1>num3)
+    {
+        Console.WriteLine(num1);
+    }
+    else
+    {
+        Console.WriteLine(num3);
+    }
+ }
+ else
+ {
+    if(num2>num3)
+        {
+        Console.WriteLine(num2);
+        }
+    else
+    {
+        Console.WriteLine(num3);
+    }
+ }
