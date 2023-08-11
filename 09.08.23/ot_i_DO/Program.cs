f57e793f3e1b2389964d6b7021ce89a875ef63a3
Konstantin <kbynevskij@yandex.ru>
2023-08-11 19:05:33 +0200

    от и до

---
+++ 09.08.23/ot_i_DO/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введение значение");
+int number = int.Parse(Console.ReadLine());
+int start = -number;
+while(start<=number)
+{
+    Console.Write(start);
+    if(start!=number)
+    {
+        Console.Write(", ");
+    }
+    start++;
+}
